c94a118e82c34a84af77ac8a954756793751ebeb
mr-d-nobody <210515471+mr-d-nobody@users.noreply.github.com>
2025-05-08 01:34:57 +0530

    Update 07.Print upto Nth Element.cpp

--- 07.Print upto Nth Element.cpp
+++ 07.Print upto Nth Element.cpp
@@ -1,34 +1,19 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int fib(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    return fib(n - 1) + fib(n - 2);
+int fib(int n){
+	if(n == 0) return 0;
+	if(n == 1) return 1;
+	return fib(n-1) + fib(n-2);
 }
 
-int main() {
-    int n;
-    cin >> n;
+int main(){
+	int n;
+	cout << "Enter the Limit: ";
+	cin >> n;
 
-    if (n <= 0 || n > 30) return 1;
-
-    int arr[30];
-    for (int i = 0; i < n; i++) {
-        arr[i] = fib(i);
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    int value = arr[n - 1];
-    if (value >= 100) return 1;
-
-    int arr1[100];
-    for (int i = 0; i <= value; i++) {
-        arr1[i] = i;
-        cout << arr1[i] << " ";
-    }
-    cout << endl;
-
-    return 0;
+	for(int i = 0; i < n; i++) {
+		cout << fib(i) << " ";
+	}
+	return 0;
 }
